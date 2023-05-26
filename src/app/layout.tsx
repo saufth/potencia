682e818da75453eda163b28c8661a959be9cd64a
saufth <saufth@gmail.com>
2023-05-26 06:57:31 -0600

    Framework version 1.1.2: robots & sitemap refactored

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export const metadata: Metadata = {
   generator: 'Next.js',
   manifest: new URL('site.webmanifest', baseUrl),
   referrer: 'origin-when-cross-origin',
+  robots: 'index, follow',
   openGraph: {
     title: organization,
     description,
