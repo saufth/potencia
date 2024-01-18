52c57e313b57f8ea67d24de64c0518a04f6c33fa
saufth <saufth@gmail.com>
2024-01-18 10:50:52 -0600

    Fix typo in organization.ts

--- src/config/organization.ts
+++ src/config/organization.ts
@@ -111,7 +111,7 @@ export const history: Article = {
     'En el año 2015, un grupo de expertos de logística y el transporte pensaron fundar una empresa diferente, que pudiera aportar soluciones innovadoras y efectivas para mejorar el desempeño empresarial de sus clientes.',
     'Así nació DISTRIBUIDORA EMPRESARIAL POTENCIA. Al comienzo de sus operaciones sortearon obstáculos que hoy en día culminan en una historia de superación y crecimiento.',
     'Sus fundadores han colaborado a lo largo de estos años con cientos de empresas que, como ellos decidieron embarcarse en la aventura de emprender. La compañía continúa trabajando incansablemente para superar las expectativas y alcanzar juntos el máximo potencial.',
-    'a historia de la empresa actualmente es el sueño cumplido de un grupo de personas altamente comprometidas por brindar un servicio de excelencia, que abarca todo el territorio nacional, enfocándose en la tecnología y la innovación, y ejerciendo eficazmente su responsabilidad social.',
+    'La historia de la empresa actualmente es el sueño cumplido de un grupo de personas altamente comprometidas por brindar un servicio de excelencia, que abarca todo el territorio nacional, enfocándose en la tecnología y la innovación, y ejerciendo eficazmente su responsabilidad social.',
     'DISTRIBUIDORA EMPRESARIAL POTENCIA es una opción confiable para todas las empresas que buscan mejorar su logística y transporte.'
   ]
 }
